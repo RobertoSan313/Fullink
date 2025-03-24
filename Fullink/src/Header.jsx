269e03fc679927cfd648fc0923367f7ddb807498
RobertoSan313 <pkgamer313@gmail.com>
2025-03-24 12:09:04 -0500

    Creacion del header

--- Fullink/src/Header.jsx
+++ Fullink/src/Header.jsx
@@ -1,7 +1,18 @@
+import './Header.css'
+
 function Header(){
     return(
         <header>
-
+            <div>
+                <button className='home'>Home</button>
+                <button>Features</button>
+                <button>Pricing</button>
+                <button>FAQs</button>
+                <button>About</button>
+            </div>
+            
         </header>
     )
 }
+
+export default Header;
